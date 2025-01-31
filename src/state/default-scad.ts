7146fc025a2cd8c1ed5eadef2319cf694df8d568
RemiIO Scarlet <i@remi.email>
2025-01-31 16:17:34 +0800

    chore: update default for glasses

--- src/state/default-scad.ts
+++ src/state/default-scad.ts
@@ -1,127 +1,243 @@
 // Portions of this file are Copyright 2021 Google LLC, and licensed under GPL2+. See COPYING.
 
-export default `/*
-  Hello there!
+export default `
+// Hi 朋友您好！ 这是一个眼镜的模型。
+// 本模型是由 OpenSCAD 语言编写的。
+/*
+## 基本形状
+// 1. 立方体
+cube([width, length, height]); // 例如: cube([10, 10, 10]);
 
-  If you're new to OpenSCAD, please learn the basics here:
-  https://openscad.org/documentation.html
+// 2. 球体
+sphere(r = radius); // 例如: sphere(r = 5);
 
-  There are lots of amazing libraries in the OpenSCAD ecosystem
-  (see this list: https://openscad.org/libraries.html).
+// 3. 圆柱体
+cylinder(h = height, r = radius); // 例如: cylinder(h = 10, r = 5);
 
-  Some of these libraries are bundled with this playground
-  (search for "demo" or "example" in the file explorer above)
-  and can be included directly from your models.
+## 基本操作
 
-  Any bugs (this is an Alpha!) or ideas of features?
-  https://github.com/openscad/openscad-playground/issues/new
+// 1. 立方体
+cube([width, length, height]); // 例如: cube([10, 10, 10]);
+
+// 2. 球体
+sphere(r = radius); // 例如: sphere(r = 5);
+
+// 3. 圆柱体
+cylinder(h = height, r = radius); // 例如: cylinder(h = 10, r = 5);
+
+## 变换
+// 1. 平移
+translate([x, y, z]) object; // 例如: translate([10, 0, 0]) cube([5, 5, 5]);
+
+// 2. 旋转（角度）
+rotate([x, y, z]) object; // 例如: rotate([0, 45, 0]) cube([5, 5, 5]);
+
+// 3. 缩放
+scale([x, y, z]) object; // 例如: scale([2, 1, 1]) cube([5, 5, 5]);
+
+## 实用技巧
+
+// 1. 模块化设计（类似函数）
+module my_shape(size) {
+    cube([size, size, size]);
+}
+my_shape(10);
+
+// 2. 循环
+for (i = [0:2:10]) {  // 从0到10，步长为2
+    translate([i, 0, 0]) cube([1, 1, 1]);
+}
+
+// 3. 条件语句
+if (size > 10) {
+    sphere(r = size);
+} else {
+    cube([size, size, size]);
+}
+
+## 常用参数
+// 1. $fn 设置圆形物体的分段数（越大越平滑）
+sphere(r = 10, $fn = 100);
+
+// 2. center 参数使物体居中
+cube([10, 10, 10], center = true);
+
+## 基本线条（使用 polygon）
+// 1. 直线
+polygon(points = [
+    [0, 0],    // 起点
+    [10, 10],  // 终点
+    [10, 11],  // 给线条一定宽度
+    [0, 1]
+]);
+
+// 2. 折线
+polygon(points = [
+    [0, 0],    // 第一个点
+    [10, 0],   // 第二个点
+    [10, 10],  // 第三个点
+    [11, 10],  // 加宽度的点
+    [11, 0],
+    [1, 0]
+]);
+## 曲线路径
+// 1. 贝塞尔曲线点集生成
+// 需要自定义模块来生成贝塞尔曲线的点
+module bezier_points(p0, p1, p2, p3, steps=10) {
+    for(t = [0:1/steps:1]) {
+        x = pow(1-t,3)*p0[0] + 
+            3*pow(1-t,2)*t*p1[0] + 
+            3*(1-t)*pow(t,2)*p2[0] + 
+            pow(t,3)*p3[0];
+        
+        y = pow(1-t,3)*p0[1] + 
+            3*pow(1-t,2)*t*p1[1] + 
+            3*(1-t)*pow(t,2)*p2[1] + 
+            pow(t,3)*p3[1];
+            
+        translate([x, y, 0])
+            circle(r=0.1);
+    }
+}
+
+## 实用技巧
+// 1. 创建一条带宽度的路径
+module path_with_width(points, width=1) {
+    // 生成偏移点来创建宽度
+    offset_points = [];
+    for(i = [0:len(points)-2]) {
+        dx = points[i+1][0] - points[i][0];
+        dy = points[i+1][1] - points[i][1];
+        len = sqrt(dx*dx + dy*dy);
+        nx = -dy/len * width/2;  // 法向量
+        ny = dx/len * width/2;
+        
+        // 添加两侧的点
+        offset_points = concat(
+            offset_points,
+            [[points[i][0]+nx, points[i][1]+ny]]
+        );
+    }
+    // 使用 polygon 连接所有点
+    polygon(points = offset_points);
+}
+
+// 2. 画圆弧
+module arc(radius, start_angle, end_angle, width = 1) {
+    difference() {
+        circle(r = radius + width/2);
+        circle(r = radius - width/2);
+        // 切除不需要的部分
+        rotate([0, 0, start_angle])
+            translate([-radius*2, 0, 0])
+                square(radius*4);
+        rotate([0, 0, end_angle])
+            translate([0, -radius*2, 0])
+                square(radius*4);
+    }
+}
+
+## 实际应用示例
+
+// 创建一个简单的草图
+module sketch() {
+    // 主要轮廓
+    polygon(points = [
+        [0, 0],
+        [20, 0],
+        [20, 10],
+        [0, 10]
+    ]);
+    
+    // 添加一些细节线条
+    translate([5, 5])
+        arc(radius = 2, start_angle = 0, end_angle = 180, width = 0.5);
+    
+    // 添加一条路径
+    path_with_width([
+        [0, 0],
+        [10, 5],
+        [20, 0]
+    ], width = 0.5);
+}
+
+// 将草图拉伸成3D
+linear_extrude(height = 5)
+    sketch();
 */
+glasses_width = 100;      // 眼镜总宽度
+temple_length = 80;       // 眼镜腿长度
+lens_distance = 8;        // 镜片间距
+bridge_width = 2;         // 鼻托宽度
+temple_width = 1;        // 眼镜腿宽度
+temple_height = 4;      // 眼镜腿高度
+temple_offset = -0.005;    // 眼镜腿位置微调
 
-title = "OpenSCAD";
+lens_size = (glasses_width - lens_distance) / 2;
+lens_height = 1;
+lens_outer_radius = lens_size/2;    // 镜片半径
+lens_inner_radius = lens_outer_radius - lens_height;  // 内圆半径
+lens_segments = 100;
 
-color("gray")
-    rotate([90, 0, 0])
-        translate([0, debug ? -60 : -20, 0])
-            linear_extrude(1)
-                text(title,
-                    halign="center",
-                    valign="center");
 
-// You can find the original for the following example in the file explorer above,
-// under openscad / examples / Basic / CSG-modules.scad
 
-// CSG-modules.scad - Basic usage of modules, if, color, $fs/$fa
-
-// Change this to false to remove the helper geometry
-debug = true;
-
-// Global resolution
-$fs=$preview ? 1 : 0.1;  // Don't generate smaller facets than 0.1 mm
-$fa=$preview ? 15 : 5;    // Don't generate larger angles than 5 degrees
-
-// Main geometry
-difference() {
-    intersection() {
-        body();
-        intersector();
+module glasses_hollow_cylinders_with_nosepad(
+    outer_r, 
+    inner_r, 
+    height, 
+    distance,            
+    bridge_width,
+    temple_w,temple_h,           
+    temple_offset,      
+    lens_segments = 100, 
+    nosepad_segments = 50
+) {
+    // 计算关键尺寸
+    center_distance = (outer_r * 2) + distance;
+    
+    // 左右镜片
+    mirror_pair() {
+        translate([center_distance/2, 0, 0])
+        hollow_cylinder(outer_r, inner_r, height, 0, lens_segments);
     }
-    holes();
-}
-
-// Helpers
-if (debug) helpers();
-
-// Core geometric primitives.
-// These can be modified to create variations of the final object
-
-module body() {
-    color("Blue") sphere(10);
-}
-
-module intersector() {
-    color("Red") cube(15, center=true);
-}
-
-module holeObject() {
-    color("Lime") cylinder(h=20, r=5, center=true);
-}
-
-// Various modules for visualizing intermediate components
-
-module intersected() {
-    intersection() {
-        body();
-        intersector();
+    
+    // 眼镜腿
+    mirror_pair() {
+        translate([(center_distance/2 + outer_r)*(1 + temple_offset), -temple_w/2, 0])
+        rotate([0, 90, 0])
+        cube([temple_length, temple_h, temple_w]);
     }
+    
+    // 鼻托
+    rotate([0, 90, 0])
+    cylinder(h = distance*1.1, r = bridge_width/2, center = true, $fn = nosepad_segments);
 }
 
-module holeA() rotate([0,90,0]) holeObject();
-module holeB() rotate([90,0,0]) holeObject();
-module holeC() holeObject();
-
-module holes() {
-    union() {
-        holeA();
-        holeB();
-        holeC();
-    }
+// 辅助模块：创建镜像对称的部件
+module mirror_pair() {
+    children();
+    mirror([1, 0, 0]) children();
 }
 
-module helpers() {
-    // Inner module since it's only needed inside helpers
-    module line() color("Black") cylinder(r=1, h=10, center=true);
-
-    scale(0.5) {
-        translate([-30,0,-40]) {
-            intersected();
-            translate([-15,0,-35]) body();
-            translate([15,0,-35]) intersector();
-            translate([-7.5,0,-17.5]) rotate([0,30,0]) line();
-            translate([7.5,0,-17.5]) rotate([0,-30,0]) line();
+module hollow_cylinder(outer_r, inner_r, height, x_pos = 0, segments = 100) {
+    translate([x_pos, 0, 0]) {
+        difference() {
+            cylinder(h = height, r = outer_r, center = true, $fn = segments);
+            cylinder(h = height + 1, r = inner_r, center = true, $fn = segments);
         }
-        translate([30,0,-40]) {
-            holes();
-            translate([-10,0,-35]) holeA();
-            translate([10,0,-35]) holeB();
-            translate([30,0,-35]) holeC();
-            translate([5,0,-17.5]) rotate([0,-20,0]) line();
-            translate([-5,0,-17.5]) rotate([0,30,0]) line();
-            translate([15,0,-17.5]) rotate([0,-45,0]) line();
-        }
-        translate([-20,0,-22.5]) rotate([0,45,0]) line();
-        translate([20,0,-22.5]) rotate([0,-45,0]) line();
     }
 }
 
-echo(version=version());
-// Written by Marius Kintel <marius@kintel.net>
-//
-// To the extent possible under law, the author(s) have dedicated all
-// copyright and related and neighboring rights to this software to the
-// public domain worldwide. This software is distributed without any
-// warranty.
-//
-// You should have received a copy of the CC0 Public Domain
-// Dedication along with this software.
-// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-`
+// 创建眼镜
+rotate([90, 0, 0])
+glasses_hollow_cylinders_with_nosepad(
+    lens_outer_radius,
+    lens_inner_radius,
+    lens_height,
+    lens_distance,
+    bridge_width,
+    temple_width,temple_height,
+    temple_offset,
+    lens_segments,
+    bridge_segments
+);`
